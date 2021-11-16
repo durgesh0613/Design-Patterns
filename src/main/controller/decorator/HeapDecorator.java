35dc2c39a6382426e6285d2dacc0139ca2e83a3c
wkdankit <ankit.pattanayak97@gmail.com>
2021-11-15 23:33:03 -0800

    Testcases and comments

--- src/main/controller/decorator/HeapDecorator.java
+++ src/main/controller/decorator/HeapDecorator.java
@@ -2,14 +2,25 @@ package main.controller.decorator;
 
 import main.controller.strategy.HeapStrategy;
 
+/**
+ * Implementing abstract decorator that implements HeapStrategy
+ * It will have children such as OddHeapDecorator as concrete decorator classes
+ */
 public abstract class HeapDecorator implements HeapStrategy {
     protected HeapStrategy heapStrategy;
 
+    /**
+     * It implements min or max heap strategy as needed.
+     * @param heapStrategy
+     */
     public HeapDecorator(HeapStrategy heapStrategy){
         this.heapStrategy = heapStrategy;
     }
 
     @Override
+    /**
+     * It calls strategy's toString method for values.
+     */
     public String toString() {
         return heapStrategy.toString();
     }
--- src/main/controller/decorator/OddHeapDecorator.java
+++ src/main/controller/decorator/OddHeapDecorator.java
@@ -6,36 +6,65 @@ import main.controller.utility.Utility;
 import main.model.TreeNode;
 
 import java.util.ArrayList;
-import java.util.Collections;
 
+/**
+ * Concrete Decorator that filters the odd elements from min or max heap.
+ */
 public class OddHeapDecorator extends HeapDecorator{
     HeapStrategy heapStrategy;
 
+    /**
+     * Initializes min or max heap strategy
+     * @param heapStrategy
+     */
     public OddHeapDecorator(HeapStrategy heapStrategy){
         super(heapStrategy);
         this.heapStrategy = heapStrategy;
     }
 
+    /**
+     * Compares the root and newNode to decide insertion pattern.
+     * @param root
+     * @param newNode
+     * @return
+     */
     @Override
     public boolean rootReplaceable(TreeNode root, TreeNode newNode) {
         return heapStrategy.rootReplaceable(root, newNode);
     }
 
+    /**
+     * Used to access root in internal classes/methods
+     * @return
+     */
     @Override
     public TreeNode getRoot() {
         return heapStrategy.getRoot();
     }
 
+    /**
+     * Used to set root in internal classes/methods.
+     * @param newNode
+     */
     @Override
     public void setRoot(TreeNode newNode) {
         heapStrategy.setRoot(newNode);
     }
 
+    /**
+     * Used to add an element to heap
+     * @param o
+     * @return
+     */
     @Override
     public boolean add(Object o) {
         return heapStrategy.add(o);
     }
 
+    /**
+     * Used to convert all heap elements to array (odd)
+     * @return
+     */
     @Override
     public Object[] toArray() {
         Object heapElements[] = heapStrategy.toArray();
@@ -47,6 +76,10 @@ public class OddHeapDecorator extends HeapDecorator{
         return Utility.filter(heapList, Constants.isOdd).toArray();
     }
 
+    /**
+     * Used to convert all elements to String (odd)
+     * @return
+     */
     @Override
     public String toString() {
         Object heapElements[] = heapStrategy.toArray();
